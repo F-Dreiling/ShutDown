cfc2b969f8957355ada0b181baab09c30daa7b1f
FX Drooklyn <dr.f380@hotmail.com>
2023-11-17 18:05:23 +0100

    Design

--- ShutDownUI/Properties/Resources.Designer.cs
+++ ShutDownUI/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace ShutDownUI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
+        internal static System.Drawing.Icon icon {
+            get {
+                object obj = ResourceManager.GetObject("icon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
         internal static System.Drawing.Icon logo {
             get {
                 object obj = ResourceManager.GetObject("logo", resourceCulture);
